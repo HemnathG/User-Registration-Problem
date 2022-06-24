c200c9363e020b1e3011cafe6e1f5d46ac4b24f9
HemnathG <hemnathdavid@gmail.com>
2022-06-24 22:05:08 +0530

    Added Sample Emails to Check the Email Regex

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.xml.validation.Validator;
+
 public class UserRegistration {
 
 	public static void checkValidFirstName(String firstName) {
@@ -25,7 +27,7 @@ public class UserRegistration {
 
 	public static void checkValidLastName(String lastName) {
 
-		boolean isLastName;
+		boolean isLastName	;
 		String lastNameRegex = "^[A-Z]{1}[a-z]{2,}$";
 		Pattern patternObject = Pattern.compile(lastNameRegex);
 		if (lastName == null) {
@@ -121,6 +123,77 @@ public class UserRegistration {
 		System.out.println();
 
 		scannerObject.close();
+		
+		System.out.println("********** Valid Sample Emails **********");
+		
+		email = "abc@yahoo.com";
+        checkValidEmail(email);
 
+        email = "abc-100@yahoo.com";
+        checkValidEmail(email);
+
+        email = "abc.100@yahoo.com";
+        checkValidEmail(email);
+
+        email = "abc111@abc.com";
+        checkValidEmail(email);
+
+        email = "abc-100@abc.net";
+        checkValidEmail(email);
+
+        email = "abc.100@abc.com.au";
+        checkValidEmail(email);
+
+        email = "abc@1.com";
+        checkValidEmail(email);
+
+        email = "abc@gmail.com.com";
+        checkValidEmail(email);
+
+        email = "abc+100@gmail.com";
+        checkValidEmail(email);
+
+        System.out.println("********** Invalid Sample Emails **********");
+
+        email = "abc";
+        checkValidEmail(email);
+
+        email = "abc@.com.my";
+        checkValidEmail(email);
+
+        email = "abc123@gmail.a";
+        checkValidEmail(email);
+
+        email = "abc123@.com";
+        checkValidEmail(email);
+
+        email = "abc123@.com.com";
+        checkValidEmail(email);
+
+        email = ".abc@abc.com";
+        checkValidEmail(email);
+
+        email = "abc()*@gmail.com";
+        checkValidEmail(email);
+
+        email = "abc@%*.com";
+        checkValidEmail(email);
+
+        email = "abc..2002@gmail.com";
+        checkValidEmail(email);
+
+        email = "abc.@gmail.com";
+        checkValidEmail(email);
+
+        email = "abc@abc@gmail.com";
+        checkValidEmail(email);
+
+        email = "abc@gmail.com.1a";
+        checkValidEmail(email);
+
+        email = "abc@gmail.com.aa.au";
+        checkValidEmail(email);
+
+    
 	}
 }
